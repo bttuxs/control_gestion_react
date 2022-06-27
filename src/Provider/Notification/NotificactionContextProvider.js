ed1c091bcbcd2b61d35d31669c031bfe0ea0cf4b
baldomero de castilla <bttuxs@gmail.com>
2022-06-27 03:11:03 -0500

    correcion de contexto para alertas

--- src/Provider/Notification/NotificactionContextProvider.js
+++ src/Provider/Notification/NotificactionContextProvider.js
@@ -2,14 +2,13 @@ import React, { useState, createContext } from 'react'
 
 export const NotificationContext = createContext();
 const configAlert = {
-    active: false,
-    message: '',
+    show: false,
+    message: 'aqui',
     type: 'info'
 }
 const NotificationContextProvider = (props) => {
     const [isGlobalSpinnerOn, setGlobalSpinner] = useState(false)
     const [isGlobalAlertOn, setGlobalAlert] = useState(configAlert)
-
     return (
         <NotificationContext.Provider value={{isGlobalSpinnerOn, setGlobalSpinner, isGlobalAlertOn, setGlobalAlert}}>
             {props.children}
