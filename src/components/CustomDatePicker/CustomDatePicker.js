f876eb48a2eb85dc23056b46546a366ce5962905
baldomero de castilla <bttuxs@gmail.com>
2022-07-15 10:59:25 -0500

    se agrega validacion en formulario para date custom

--- src/components/CustomDatePicker/CustomDatePicker.js
+++ src/components/CustomDatePicker/CustomDatePicker.js
@@ -5,13 +5,8 @@ import "react-datepicker/dist/react-datepicker.css";
 const CustomDatePicker = (props) => {
     const [startDate, setStartDate] = React.useState(props.date);
     const ExampleCustomInput = React.forwardRef(({ value, onClick }, ref) => (
-        <button
-        type="button"
-        className="w-full px-3 py-2 text-sm h-9 leading-tight text-gray-700 rounded outline outline-1 outline-gray-500" 
-        onClick={onClick} 
-        ref={ref}>
-            {value}
-        </button>
+        <input type="text" ref={ref} onClick={onClick} value={value} required={props.required} className="w-full px-3 py-2 text-sm h-9 leading-tight text-gray-700 rounded "  />
+
     ));
     function setData(date){
         setStartDate(date)
@@ -20,6 +15,7 @@ const CustomDatePicker = (props) => {
     return (
         <DatePicker
         dateFormat="dd/MM/yyyy"
+        required
         selected={startDate}
         onChange={(date) => setData(date)}
         customInput={<ExampleCustomInput />}
